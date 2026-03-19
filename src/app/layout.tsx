603a215c53f9cf60ed1c65ca5ee87570233f9a10
als8921 <als8921@naver.com>
2026-03-19 15:51:10 +0900

    fix: webcrx 문제 안뜨게 하기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" suppressHydrationWarning>
       <body className="bg-gray-50 min-h-screen">{children}</body>
     </html>
   );
